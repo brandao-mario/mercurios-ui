816701fdc7530f40eb2bc10a885fa719c89a9aef
Mário Brandão <mariobrandaobr@hotmail.com>
2015-05-11 19:07:03 -0300

    botões, sorteio de bg

---
+++ assets/modules/mercurios-js/mercurios.js
@@ -0,0 +1,26 @@
+var opcoes = 
+
+background = {
+	_bg: [],
+	urls: ["url('assets/img/bg1.jpg') no-repeat",
+	"url('assets/img/bg2.jpg') no-repeat",
+	"url('assets/img/bg3.jpg') no-repeat",
+	"url('assets/img/bg4.jpg') no-repeat"],
+
+	// Sorteia o BG
+	sorteia: function() {
+		this._bg.push({
+			url: this.urls[Math.floor(4 * Math.random())]
+		});
+	},
+	// Insere no body
+	insere: function() {
+		for (var i = 0, tam = this._bg.length; i < tam; i++) {
+			var bg = this._bg[i];
+			document.body.style.background = bg.url;
+		}
+	}
+}
+
+background.sorteia();
+background.insere();
