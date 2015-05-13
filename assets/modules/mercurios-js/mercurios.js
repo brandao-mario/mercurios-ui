f53cd450bd57cf49c6b56b34600da66d9a41b99b
Mário Brandão <mariobrandaobr@hotmail.com>
2015-05-13 19:14:53 -0300

    Slider com slick-js

--- assets/modules/mercurios-js/mercurios.js
+++ assets/modules/mercurios-js/mercurios.js
@@ -1,26 +1,37 @@
-var opcoes = 
+$(document).ready(function(){
 
-background = {
-	_bg: [],
-	urls: ["url('assets/img/bg1.jpg') no-repeat",
-	"url('assets/img/bg2.jpg') no-repeat",
-	"url('assets/img/bg3.jpg') no-repeat",
-	"url('assets/img/bg4.jpg') no-repeat"],
+	// Background randomico
+	background = {
+		_bg: [],
+		urls: ["url('assets/img/bg1.jpg') no-repeat",
+		"url('assets/img/bg2.jpg') no-repeat",
+		"url('assets/img/bg3.jpg') no-repeat",
+		"url('assets/img/bg4.jpg') no-repeat"],
 
-	// Sorteia o BG
-	sorteia: function() {
-		this._bg.push({
-			url: this.urls[Math.floor(4 * Math.random())]
-		});
-	},
-	// Insere no body
-	insere: function() {
-		for (var i = 0, tam = this._bg.length; i < tam; i++) {
-			var bg = this._bg[i];
-			document.body.style.background = bg.url;
+		// Sorteia o BG
+		sorteia: function() {
+			this._bg.push({
+				url: this.urls[Math.floor(4 * Math.random())]
+			});
+		},
+		// Insere no body
+		insere: function() {
+			for (var i = 0, tam = this._bg.length; i < tam; i++) {
+				var bg = this._bg[i];
+				$('.bg-random').css("background",bg.url);
+			}
 		}
 	}
-}
 
-background.sorteia();
-background.insere();
+	background.sorteia();
+	background.insere();
+
+	// Affix
+	
+	if ($('.affix').y == 0) {
+		$(this).css("position","fixed");
+	} else{
+
+	};
+
+});
